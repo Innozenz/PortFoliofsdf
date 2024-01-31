aade1cbcd0d68fd2bf2c620662089e03430e176b
Di Majo Wilsfried <wilsfrieddm@gmail.com>
2024-01-31 15:52:58 +0100

    Page 2 components Skills added & Projects changed

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import HTML from "../../Assets/Html.webp"
+import CSS from "../../Assets/CSS.png"
+import ReactLogo from "../../Assets/React.webp"
+import ReduxLogo from "../../Assets/Redux.png"
+import SASSLogo from "../../Assets/Sass.webp"
+import ViteLogo from "../../Assets/Vite.webp"
+
+export default function Skills() {
+    const skillsData = [
+      { name: 'HTML', imgSrc: HTML, alt: 'HTML Logo', gridRow: 1 },
+      { name: 'CSS', imgSrc: CSS, alt: 'CSS Logo', gridRow: 2 },
+      { name: 'SASS', imgSrc: SASSLogo, alt: 'SASS Logo', gridRow: 3 },
+      { name: 'React', imgSrc: ReactLogo, alt: 'React Logo', gridRow: 4 },
+      { name: 'Vite', imgSrc: ViteLogo, alt: 'Vite Logo', gridRow: 5 },
+      { name: 'Redux', imgSrc: ReduxLogo, alt: 'Redux Logo', gridRow: 6 },
+    ];
+    
+    const screenWidth = window.innerWidth;
+
+    const increment = screenWidth > 650 ? 2 : 1;
+
+    const getRandomColumn = () => {
+      let randomColumn;
+      do {
+        randomColumn = Math.floor(Math.random() * 4) + increment; 
+      } while (randomColumn === 1 || randomColumn === 6);
+      return randomColumn;
+    };
+  
+    return (
+    <>
+      <h2 className='Skills'>Les Technologies que j'ai déjà utilisées :</h2>
+      <div className='SkillsWrapper'>
+        {skillsData.map((skill, index) => (
+          <div key={index} className='SkillContainer' style={{ gridColumn: getRandomColumn(), gridRow: skill.gridRow,  }}>
+            <img
+              className='SkillImg'
+              src={skill.imgSrc}
+              alt={skill.alt}
+            />
+            <p className='SkillName'>{skill.name}</p>
+          </div>
+        ))}
+      </div>
+    </>
+    );
+  }
+  
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,17 +2,18 @@ import React from "react";
 
 /* Components imported */
 import CustomCursor from "../components/CustomCursor/CustomCursor";
-import Stars from "../components/Stars/Stars"
-import Project from "../components/Projects/Project"
+import Stars from "../components/Stars/Stars";
+import Project from "../components/Projects/Project";
+import Skills from "../components/Skills/Skills";
 /* Components imported */
 
 /* JSON Data */
-import jsonData from "../components/JsonData/Project.json"
+import jsonData from "../components/JsonData/Project.json";
 /* */
 
 /* Utilities */
-import Projet8 from "../Assets/Appartement8.webp"
-import Projet11 from "../Assets/Bank11.webp"
+import Projet8 from "../Assets/Appartement8.webp";
+import Projet11 from "../Assets/Bank11.webp";
 /* Utilities */
 
 export default function Projects() {
@@ -23,6 +24,7 @@ export default function Projects() {
       <main className="flexboxProjects">
         <Project projectData={{ ...jsonData[0], pictures: [Projet8] }} />
         <Project projectData={{ ...jsonData[1], pictures: [Projet11] }} />
+        <Skills />
       </main>
       <footer></footer>
     </div>
