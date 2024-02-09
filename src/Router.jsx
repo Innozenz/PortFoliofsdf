8ff3b4a2c4e0aa0ce6b7fffc9278d42f0d65f49a
Di Majo Wilsfried <wilsfrieddm@gmail.com>
2024-02-09 13:34:40 +0100

    import build + fix

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Landing from "./pages/Landing"
 import Projects from "./pages/Projects" 
-/* import Landing from "./pages/Landing"  Add the page later */
 
 import './styles/Main.css'
 
@@ -12,7 +11,6 @@ function App() {
         <Routes>
           <Route path='/' element={<Landing />} />
           <Route path='/projects' element={<Projects />} />
-          {/*  <Route path='/user' element={<User />} />  Add the page later */}
         </Routes>
       </Router>
   );
