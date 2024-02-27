1021ac66d5ee2db56f8a606cccfb479398805c83
Di Majo Wilsfried <wilsfrieddm@gmail.com>
2024-02-27 11:34:43 +0100

    changing name of main SASS file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./Router.jsx"
 
-import './styles/Main.scss'
+import './styles/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
