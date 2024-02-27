c5d7a0b6d6d1708c899bdd7e60015d80d9cf8f3d
Di Majo Wilsfried <wilsfrieddm@gmail.com>
2024-02-27 11:16:48 +0100

    Install SASS dependency

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./Router.jsx"
 
-import './styles/Main.css'
+import './styles/Main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
